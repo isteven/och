390b3fbf912341cedb1d977fc900dce649540920
Steven <steven@bitbucket.org>
2016-07-18 19:01:38 +0800

    validate language (now only accept en and zh)

--- controller.js
+++ controller.js
@@ -495,7 +495,10 @@ myApp.controller('bodyCtrl', ['$scope', '$http', '$sce', function($scope, $http,
 
     var tempLang = getUrlVar( 'lang' );
     if ( tempLang ) {
-        $scope.lang = tempLang;
+        $scope.lang = tempLang.toLowerCase();
+        if ( $scope.lang != 'en' && $scope.lang != 'zh' ) {
+            $scope.lang = 'en';
+        }
     }
 
     $scope.tncToggle = function() {
